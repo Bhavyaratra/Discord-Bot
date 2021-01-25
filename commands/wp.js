762b4a1acc68ef8acdf03a61024af54b1e5dce7f
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-01-26 04:25:03 +0530

    removed atleast

--- commands/wp.js
+++ commands/wp.js
@@ -5,7 +5,7 @@ module.exports= async function(msg , args){
 
     const fetch = require("node-fetch");
 
-    let url= `https://wallhaven.cc/api/v1/search?q=${keyword}&purity=111&atleast=1920x1080&apikey=${process.env.WALL_KEY}`;
+    let url= `https://wallhaven.cc/api/v1/search?q=${keyword}&purity=111&apikey=${process.env.WALL_KEY}`;
     let response = await fetch(url);
     let json = await response.json();
     console.log(json); 
