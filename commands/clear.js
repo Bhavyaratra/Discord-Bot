544afcc330d21668ba577f8760072cc7f1d747de
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-01-26 01:51:07 +0530

    gifs embedded

--- commands/clear.js
+++ commands/clear.js
@@ -1,10 +1,14 @@
 module.exports=function(msg, s){
 var n=1;
 n=s[0];
+if(n==null)
+{
+  n=1;
+}
 n=Number(n)+1;  
   console.log(n);
   async function clear(){
-  
+  console.log('cleared');
   await msg.channel.bulkDelete(n)  
   }   
 clear()     
--- commands/help.js
+++ commands/help.js
@@ -3,16 +3,19 @@ const Discord = require('discord.js');
 
 module.exports = function(msg , args){
     const embedex = new Discord.MessageEmbed()
-    .setColor('#9999f')
-    .setTitle('commands')
-    .setThumbnail('https://imgur.com/vtjO5K3.png')
+    .setColor('#00FFFF')
+    .setAuthor('commands','https://imgur.com/vtjO5K3.png')
+    .setThumbnail('https://imgur.com/cb1Npaw.gif')
     .setDescription('Prefix: $')
     .addFields(
          {name: 'clear', value: '$clear <int>' ,inline:false},
-         {name: 'gif', value: '$gif <keyword>', inline:false}
+         {name: 'gif', value: '$gif <keyword>', inline:false},
+         { name: '\u200B', value: '\u200B' }
 
     )
-    .setTimestamp();
+    .setImage('https://i.imgur.com/GhO5vPn.gif')
+    .setTimestamp()
+    .setFooter('GG');
 
 
     msg.channel.send(embedex);
